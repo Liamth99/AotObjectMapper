848561f28ca04c3b877142207c974c8f49b175c5
Liam <liamth@live.com.au>
2026-02-05 11:02:18 +1000

    Added Brism for code blocks

--- docs/AOMDocs.Source/Program.cs
+++ docs/AOMDocs.Source/Program.cs
@@ -1,9 +1,13 @@
 using AOMDocs.Source;
+using Brism;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddBrism();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
