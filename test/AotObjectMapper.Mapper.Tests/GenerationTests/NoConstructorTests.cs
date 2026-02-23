3ef6d4cc6375c0ae9aaa261428ccac2e60b1b5c1
Liam Thompson <liamth@live.com.au>
2026-02-23 17:16:35 +1000

    Add unit test for `AOM207` diagnostic validation in `NoConstructorTests` when no empty constructor avaliable

--- test/AotObjectMapper.Mapper.Tests/GenerationTests/NoConstructorTests.cs
+++ test/AotObjectMapper.Mapper.Tests/GenerationTests/NoConstructorTests.cs
@@ -38,6 +38,37 @@ public class NoConstructorTests : AOMVerifierBase
     }
 
     [Fact]
+    public async Task MapAttribute_NoEmptyCtor_AOM207()
+    {
+        const string code =
+        """
+        using System;
+        using AotObjectMapper.Abstractions.Attributes;
+        using AotObjectMapper.Abstractions.Enums;
+        using AotObjectMapper.Abstractions.Models;
+        
+        namespace TestGen
+        {
+            public class T1;
+            
+            public class T2
+            {
+                public T2(string s)
+                {
+            
+                }
+            }
+            
+            [GenerateMapper]
+            [Map<T1, {|#0:T2|}>]
+            public partial class NoCtorMapper;
+        }
+        """;
+
+        await VerifyGeneratorDiagnosticsAsync(code, [DiagnosticResult.CompilerError(AOMDiagnostics.NoConstructorId).WithLocation(0).WithArguments("T2")], TestContext.Current.CancellationToken);
+    }
+
+    [Fact]
     public async Task MapAttribute_NoPublicCtorWithFactory_NoDiagnostic()
     {
         const string code =
